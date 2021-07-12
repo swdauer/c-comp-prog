9a9cf006a0b2b6fdb32ac081c3f488fa6bac59b7
Scott Dauer <swdauer@gmail.com>
2021-07-11 21:06:35 -0500

    add reading and recursion of minInsertions if n10 > 0

--- coke/main.c
+++ coke/main.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 
+int minInsertions(int c, int n1, int n5, int n10)
+{
+  if (n10 > 0)
+  {
+    return minInsertions(c - n10, n1 + 2 * n10, n5, 0) + n10;
+  }
+}
+
 int main()
 {
-  printf("test\n");
+  int n;
+  scanf(" %d", &n);
+  int i;
+  for (i = 0; i < n; i++)
+  {
+    int c, n1, n5, n10;
+    scanf(" %d %d %d %d", &c, &n1, &n5, &n10);
+    printf("%d\n", minInsertions(c, n1, n5, n10));
+  }
   return 0;
 }
